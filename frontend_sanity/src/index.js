975798ce7179eb5b0129a3c4651e654190861362
kbj2077 <1275777632@qq.com>
2023-02-20 15:27:24 +0800

    feat: 基本框架搭建

---
+++ frontend_sanity/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import App from "./App";
+import "./index.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+ReactDOM.render(
+  <Router>
+    <GoogleOAuthProvider>
+      <App />
+    </GoogleOAuthProvider>
+  </Router>,
+  document.getElementById("root")
+);
